3009543743ec11a90114c258afefadb578a533cc
erkin <erkinbatu@gmail.com>
2018-05-01 17:44:00 +0300

    v0.6 Check the whole expression beforehand Proper exact-integer handling

--- src/calc.scm
+++ src/calc.scm
@@ -1,34 +1,56 @@
 (module rpn (calculate operators)
   (import chicken scheme)
+  
   (define operators '((+ . +) (- . -) (* . *) (/ . quotient)
-                   (^ . expt) (% . remainder)))
-
+                      (^ . expt) (% . remainder)))
+  
   (define (err message code)
     (print "Error: " message)
     (exit code))
+
+  (define (int-check value)
+    (let ((num (string->number value)))
+      (if (and (integer? num) (not (inexact? num)))
+          num
+          (err (string-append "Not an exact integer: " value) 2))))
+
+  (define (sym-check value)
+    (if (assoc (string->symbol value) operators)
+        (string->symbol value)
+        (err (string-append "Unrecognised token: " value) 2)))
+
+  (define (exp-check exp new-exp)
+    (if (pair? exp)
+        (let ((value (car exp)))
+          (exp-check
+           (cdr exp)
+           `(,@new-exp
+             ,(if (string->number value)
+                  (int-check value)
+                  (sym-check value)))))
+        new-exp))
   
   (define (work token stack)
-    (if (null? stack)
-        (err "Stack empty." 1))
-    (let ((operator (assoc token operators)))
-      (if operator
-          (if (null? (cdr stack))
-              (err "Stack too short." 1)
-              (cons ((eval (cdr operator)) (cadr stack) (car stack)) (cddr stack)))
-          (err "Invalid token." 2))))
-
+    (cond
+     ((integer? token)
+      `(,@stack ,token))
+     ((null? stack)
+      (err "Stack empty." 1))
+     ((null? (cdr stack))
+      (err "Stack too short." 1))
+     (else
+      (cons
+       ((eval (cdr (assoc token operators))) (car stack) (cadr stack))
+       (cddr stack)))))
+  
   (define (calc exp stack step)
     (print step ": " exp " ⇒ " stack)
-    (if (not (null? exp))
-        (let ((token (string->number (car exp))))
-          (if token
-              (if (integer? token)
-                  (calc (cdr exp) (cons token stack) (+ 1 step))
-                  (err "Value not an integer." 3))
-              (calc (cdr exp) (work (string->symbol (car exp)) stack) (+ 1 step))))
-        stack))
+    (if (null? exp)
+        stack
+        (let ((token (car exp)))
+          (calc (cdr exp) (work token stack) (+ 1 step)))))
 
   (define (calculate exp)
     (print "Input: " exp)
-    (print "Output: " (calc exp '() 0))
+    (print "Output: " (calc (exp-check exp '()) '() 0))
     (exit)))
