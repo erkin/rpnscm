82622192e9ff877ea57027dc82b897b0155f6e9e
erkin <erkinbatu@gmail.com>
2018-05-30 22:35:24 +0300

    Add comments for operators Fix # bug We need to find better symbols for operators

--- src/op.scm
+++ src/op.scm
@@ -1,62 +1,121 @@
 (module rpn-op (rpn:eval rpn:operators)
   (import chicken scheme)
 
-  ;;; Monadic
-  (define (rpn:neg stack)
+  ;;; Monadic operators
+  ;; negate
+  ;; 3 n -5 n
+  ;; (-3 5)
+  (define (rpn:neg stack) 
     `(,(- (car stack)) ,@(cdr stack)))
 
+  ;; absolute value
+  ;; 3 @ -5 @
+  ;; (3 5)
   (define (rpn:abs stack)
     `(,(abs (car stack)) ,@(cdr stack)))
 
+  ;; duplicate
+  ;; 5 :
+  ;; (5 5)
   (define (rpn:dup stack)
     `(,(car stack) `(car stack) ,@(cdr stack)))
   
+  ;; pop
+  ;; 3 5 !
+  ;; "5"
+  ;; (3)
   (define (rpn:pop stack)
     (print (car stack))
     (cdr stack))
 
+  ;; peek
+  ;; 3 5 ?
+  ;; "5"
+  ;; (3 5)
+  (define (rpn:pek stack)
+    (print (car stack))
+    stack)
+
+  ;; sign
+  ;; 3 s -5 s
+  ;; (1 -1)
   (define (rpn:sig stack)
     `(,(signum (car stack)) ,@(cdr stack)))
   
+
   ;;; Dyadic
+  ;; addition
+  ;; 3 5 + -3 5 +
+  ;; (8 2)
   (define (rpn:add stack)
     `(,(+ (car stack) (cadr stack)) ,@(cddr stack)))
 
+  ;; subtraction
+  ;; 5 3 - 3 5 - -3 5 -
+  ;; (2 -2 -8)
   (define (rpn:sub stack)
     `(,(- (car stack) (cadr stack)) ,@(cddr stack)))
 
+  ;; multiplication
+  ;; 3 -5 *
+  ;; (-15)
   (define (rpn:mul stack)
     `(,(* (car stack) (cadr stack)) ,@(cddr stack)))
 
+  ;; [integer] division
+  ;; -20 5 / 5 3 /
+  ;; (-4 1)
   (define (rpn:div stack)
     `(,(quotient (car stack) (cadr stack)) ,@(cddr stack)))
 
+  ;; exponentiation
+  ;; 3 5 ^ 5 3 ^
+  ;; (243 125)
   (define (rpn:exp stack)
     `(,(expt (car stack) (cadr stack)) ,@(cddr stack)))
 
+  ;; [integer] modulo
+  ;; 3 5 % 5 3 %
+  ;; (3 2)
   (define (rpn:mod stack)
     `(,(remainder (car stack) (cadr stack)) ,@(cddr stack)))
 
+  ;; swap
+  ;; 3 5 ~
+  ;; (5 3)
   (define (rpn:swp stack)
     `(,(cadr stack) (car stack) ,@(cddr stack)))
 
-  ;; Polyadic
+  ;;; Polyadic
+  ;; minimum
+  ;; 7 -3 5 m
+  ;; (-3)
   (define (rpn:min stack)
     (list (min stack)))
   
+  ;; maximum
+  ;; 7 -3 5 M
+  ;; (7)
   (define (rpn:max stack)
     (list (max stack)))
   
+  ;; summation
+  ;; 7 -3 5 S
+  ;; (9)
   (define (rpn:sum stack)
     (list (apply + stack)))
   
+  ;; product
+  ;; 7 -3 5 M
+  ;; (-105)
   (define (rpn:pro stack)
     (list (apply * stack)))
 
+
   ;; alists of procedures defined above
-  (define rpn:monadic  `((# . ,rpn:neg) (a . ,rpn:abs)
+  (define rpn:monadic  `((n . ,rpn:neg) (@ . ,rpn:abs)
                          (: . ,rpn:dup) (! . ,rpn:pop)
-                         (s . ,rpn:sig)               ))
+                         (s . ,rpn:sig) (? . ,rpn:pek))
   (define rpn:dyadic   `((+ . ,rpn:add) (- . ,rpn:sub)
                          (* . ,rpn:mul) (/ . ,rpn:div)
                          (^ . ,rpn:exp) (% . ,rpn:mod)
